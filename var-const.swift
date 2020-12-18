aacef51a6c1bbb91a644957a79cb48f468b347a7
Andres Chavez <andres.chavez.crisostomo@gmail.com>
2020-12-18 18:26:28 -0500

    Create var-const.swift

---
+++ var-const.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+var str = "Hello, playground"
+
+// MARK: - Contastantes --> Su Valor No Cambia
+
+let edadPersona = 32                  //Definicion Implicita
+let nombrePerson: String = "Andres"     //Definicion Explicita
+
+//edadPersona = edadPersona + 2
+
+// MARK: - Variable --> Puedo Cambiar su Valor!!
+
+var edadAlumno = 25
+var edadProfesor: Double = 30
+edadAlumno = edadAlumno + 2
+edadProfesor += 0.5

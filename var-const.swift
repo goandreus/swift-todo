81c59d17db58eb0334c906d431d4a0d3b6429d31
Andres Chavez <andres.chavez.crisostomo@gmail.com>
2020-12-21 01:18:26 -0500

    Update var-const.swift

--- var-const.swift
+++ var-const.swift
@@ -12,6 +12,32 @@ let nombrePerson: String = "Andres"     //Definicion Explicita
 // MARK: - Variable --> Puedo Cambiar su Valor!!
 
 var edadAlumno = 25
-var edadProfesor: Double = 30
-edadAlumno = edadAlumno + 2
-edadProfesor += 0.5
+var edadProfesor = 30
+
+let nuevaEdsdProfesor = Double (edadProfesor) + 0.5
+
+// NIL = es NULO
+// MARK: - OPTIONSL ? - este valor puede ser nulo(nil)
+
+let edadTexto = "26"
+var edad: Int? = Int(edadTexto)
+
+// ! --> permite obtener el valor de un OPTIONAL (UNWRAP)
+
+if  edad != nil {
+    print("Mi edad es: \(edad!)")
+}
+
+
+// MARK: - FUNCIONES
+// func nombreFuncion(ALIAS1 variable1: Tipo1, ALIAS2 variable2:Tipo2) -> TipoRetorno
+
+func imprimir(unTexto text:String) /* -> Void */ {
+    print(text)
+}
+imprimir(unTexto: "Hola gente")
+
+func dividir(dividendo a:Int, divisor b:Int) -> Int {
+    return a/b
+}
+let cociente = dividir(dividendo:12, divisor:4)

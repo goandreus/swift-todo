c3d84767a37ec0807879e4bed2b6c1cc094732ea
Andres Chavez <andres.chavez.crisostomo@gmail.com>
2021-01-04 11:46:13 -0500

    Update basic.swift

--- basic.swift
+++ basic.swift
@@ -2,153 +2,158 @@ import UIKit
 
 var str = "Hello, playground"
 
-// MARK: - Contastantes --> Su Valor No Cambia
-
-let edadPersona = 32                  //Definicion Implicita
-let nombrePerson: String = "Andres"     //Definicion Explicita
+// MARK: - Constantes LET --> SU VALOR NO CAMBIA!
+let edadPersona = 32                    // Definición Implicita
+let nombrePersona: String = "Andres"    // Definición Explicita
 
 //edadPersona = edadPersona + 2
 
-// MARK: - Variable --> Puedo Cambiar su Valor!!
+// MARK: - Variable VAR --> PUEDO CAMBIAR SU VALOR!!
+var edadAlumno = 25                     // Definición Implicita
+var edadProfesor: Int = 31           // Definición Explicita
 
-var edadAlumno = 25
-var edadProfesor = 30
+edadAlumno += 2
+edadProfesor += 5
 
-let nuevaEdsdProfesor = Double (edadProfesor) + 0.5
+let nuevaEdadProfesor = Double(edadProfesor) + 0.5
 
 // NIL = es NULO
-// MARK: - OPTIONSL ? - este valor puede ser nulo(nil)
-
-let edadTexto = "26"
+// MARK: - OPTONAL ? --> Este valor puede ser NULO (NIL)
+let edadTexto = "32"
 var edad: Int? = Int(edadTexto)
 
-// ! --> permite obtener el valor de un OPTIONAL (UNWRAP)
-
-if  edad != nil {
+// ! --> permite obtener el valor de un OPTINAL (UNWARP)
+if edad != nil {
     print("Mi edad es: \(edad!)")
+} else {
+    print("No se puede convertir la edad texto a entero")
 }
 
+// MARK: - Funciones
+//
+// func nombreFuncion(ALIAS1 variable1:Tipo1, ALIAS2 variable2:Tipo2) -> TipoRetorno
+//
 
-// MARK: - FUNCIONES
-// func nombreFuncion(ALIAS1 variable1: Tipo1, ALIAS2 variable2:Tipo2) -> TipoRetorno
-
-func imprimir(unTexto text:String) /* -> Void */ {
-    print(text)
+func imprimir(unTexto texto:String) /* -> Void */ {
+    print(texto)
 }
-imprimir(unTexto: "Hola gente")
+imprimir(unTexto: "Hola chicos bienvenidos a Swift")
 
-func dividir(dividendo :Int, divisor :Int) -> Int { // EL ALIAS ES LA VARIABLE
-    return dividendo/divisor
+
+func dividir(dividendo:Int, divisor:Int) -> Int { // El ALIAS ES LA VARIABLE
+    return dividendo / divisor
 }
-let cociente = dividir(dividendo:12, divisor:4)
+let cociente = dividir(dividendo: 12, divisor: 5)
 
-func suma(_ A:Int, _ B:Int) -> Int{
+// El _ significa que no tendremos alias
+func suma(_ A:Int, _ B:Int) -> Int {
     return A + B
 }
+let sumaRpta = suma(1, 2)
 
-let sumaRpta = suma (1, 2)
 
-//MARK: - Clases  no es una es STRUCT
-
-class Persona {
-    //Propiedades
-    var nombre: String = ""
-    
-    //Metodos
-    func imprimirDatos() {
-        print("\(nombre)")
-    }
-}
+// MARK: - Clases NO ES Struct
 
 // STRUCT
 // --> Es mucho mas rapido que una clase
-// --> Va por Valor
-// Una STRUC no hereda, A menos que sea un PROTOCOL
-
+// --> Va por VALOR
+// --> No tiene herencia. A menos que sea un PROTOCOL
 struct Alumno {
+    // Propiedades
     var nombre: String = ""
     var edad: Int = 0
     
-    func imprimirDatos(){
+    // Propiedad calculada --> Computed Property --> GET ONLY
+    var edadEn10anhos: Int {
+        return edad + 10
+    }
+    
+    // Metodos
+    func imprimirDatos() {
         print("\(nombre) \(edad)")
     }
 }
 
 var estudiante = Alumno()
-estudiante.nombre = "Andres"
-//CLASS
-// --> Es mas lento que un STRUCT
-// --> var por referencia
-// --> Tienen herencia de class o protocol
+estudiante.nombre = "Ismael"
 
-class Profesor: Persona{
-    //Propiedades
+// CLASS
+// --> Es mas lento que un STRUCT
+// --> var por refrencia
+// --> Tienen herencia de Class o Procol
+class Persona {
+    
+    // Propiedades
+    var nombre: String = ""
+    
+    // Metodo
+    func imprimirDatos() {
+        print("\(nombre)")
+    }
+}
+
+class Profesor: Persona {
+    // Propiedades
     var edad: Int = 0
     
-    // Sobreescribir
     override func imprimirDatos() {
-        print("\(edad)")
+        print("\(nombre) \(edad)")
     }
-    
 }
-let tutor  = Profesor()
+
+let tutor = Profesor()
 tutor.nombre = "Andres"
 
 class Curso {
+    
+    // Propiedes
     var titulo: String
     var maestro: Profesor
     var estudiantes: [Alumno]
     
-    //CONSTRUCTOR --> INIT
-    
-    init(){
-        
-        //this == self
+    // CONSTRUCTOR --> INIT
+    init() {
+        // this == self
         self.titulo = ""
         self.maestro = Profesor()
         self.estudiantes = []
     }
     
-    init (titulo: String,maestro: Profesor, estudiantes: [Alumno]){
+    init(titulo: String, maestro: Profesor, estudiantes: [Alumno]) {
         self.titulo = titulo
         self.maestro = maestro
         self.estudiantes = estudiantes
     }
 }
 
-let iosBasico = Curso(titulo: "ios", maestro: tutor, estudiantes: [estudiante])
+let iosBasico = Curso(titulo: "iOS", maestro: tutor, estudiantes: [estudiante])
 let unCurso = Curso()
 
-//MARK -  IF LET -- SE UTILIZA PARA VALIDAR OPTINAL
-
+// MARK: - IF LET -- SE UTILIZA PARA VALIDAR OPTINAL
 let numeroString = "123"
 
-//! --> permite obtener el valor de un OPTIONAL (UNWARP)
-
-//version 1
+// Version 1
 let numeroConvertido = Int(numeroString)
 if numeroConvertido != nil {
     let numeroUNWRAP = numeroConvertido!
-    print("v1 El numero es: \(numeroUNWRAP)")
+    print("v1 El número es: \(numeroUNWRAP)")
 }
 
-//version 2
+// Version 2
 let numeroConvertido2 = Int(numeroString)
-if let numeroUNWRAP = numeroConvertido2{
-    print("v2 El numero es: \(numeroUNWRAP)")
+if let numeroUNWRAP = numeroConvertido2 {
+    print("v2 El número es: \(numeroUNWRAP)")
 }
 
-//version 3
-if let numeroUNWRAP = Int(numeroString){
-    print("v3 El numero es: \(numeroUNWRAP)")
+// Version 3
+if let numeroUNWRAP = Int(numeroString) {
+    print("v3 El número es: \(numeroUNWRAP)")
 }
 
-//MARk: - GUARD -- SE UTILIZA PARA VALIDAR OPTIONAL
-
-func convertirEntero(elTexto texto: String) -> Int{
-    //Patron Dorado --> Valida lo que no la ruta critica, si no de la ruta critica haz un return
-    
-    if texto.isEmpty{
+// MARK: - GUARD -- SE UTILIZA PARA VALIDAR OPTINAL
+func converirAEntero(elTexto texto:String) -> Int {
+    // Patron Dorado --> Valida lo que no es el ruta critica, si no es de la ruta critica haz un return
+    if texto.isEmpty {
         return 0
     }
     if texto.contains(".") {
@@ -158,20 +163,174 @@ func convertirEntero(elTexto texto: String) -> Int{
         return 0
     }
     return numero
-
-    
-    
-//    if !texto.isEmpty {
-//        if !texto.contains("."){
-//            if let numero = Int (texto){
-//                return numero
-//            }else {
-//                return 0
-//            }
-//        }else {
-//            return 0
-//        }
-//    }else {
-//        return 0
-//    }
 }
+
+// MARK: - FOR --> siempre se ejecuta como un FOR EACH
+// for (int i=0; i<10; i++) <--- NO EXISTE EN SWIFT
+// i++ <---- NO existe en SWIFT
+var i = 0
+i += 1
+let final = 4
+
+let range = 0..<10      // Range<Int> que omite el último valor
+let range2: ClosedRange<Int> = i...final  // ClosedRange<Int> porque cuenta al último valor
+for indice in range2 {
+    print(indice)
+}
+
+let nombreProfesor = "Andres Chavez"
+let endIndex = nombreProfesor.index(nombreProfesor.startIndex, offsetBy: 4)
+nombreProfesor[nombreProfesor.startIndex...endIndex]
+
+// MARK: - SWITCH
+let asistenciaEnClase = 4
+switch asistenciaEnClase {
+case 0..<4:
+    print("Jalo el curso por inasistencias")
+case 4...8:
+    print("Debe dar un examen")
+default:
+    print("Valor incorrecto")
+}
+
+let simbolo = "+"
+switch simbolo {
+case "+":
+    print("Suma")
+case "-":
+    print("Resta")
+case "*":
+    print("Multiplicación")
+case "/":
+    print("División")
+default:
+    print("NO ES OPERACIÓN")
+}
+
+// MARK: - ENUMS --> EN EL FONDO ES UN STRUCT
+// No tiene propideades de almacenamiento
+// Si tiene propiedades calculadas (GET ONLY o Computed Properties)
+// Si tiene Métodos (func)
+
+// EMNUM TIPO 1 --> El enum SIMPLE o ENUM VAINILLA
+enum DiaDeSemana {
+    case lunes
+    case martes
+    case miercoles
+    case jueves
+    case viernes
+    case sabado
+    case domingo
+}
+
+func almuerzo(delDia dia: DiaDeSemana) -> String {
+    switch dia {
+    case .lunes, .miercoles:
+        print("Dias de cocina rapida")
+        return "Tallarines Rojos"
+        
+    case .martes:
+        print("Dia de menesta y pescado")
+        return "Frejoles con pescado"
+        
+    case .jueves:
+        return "Jueves de Pavita"
+        
+    case .viernes:
+        return "Lomo Saltado"
+        
+    case .sabado, .domingo:
+        return "Pollo a la brasa"
+    }
+}
+
+var ayer = DiaDeSemana.lunes        // Definición Implicita
+var hoy: DiaDeSemana = .martes      // Definición explicita
+let plato = almuerzo(delDia: .martes)
+
+// EMNUM TIPO 2 --> El enum RAW VALUE
+enum Operacion: String {
+    case suma = "+"
+    case resta = "-"
+    case multiplicacion = "*"
+    case division = "/"
+    
+    func operar(numeroA a:Int, numeroB b:Int) -> Int {
+        switch self {
+        case .suma:
+            return a + b
+        case .resta:
+            return a - b
+        case .multiplicacion:
+            return a * b
+        case .division:
+            return a / b
+        }
+    }
+}
+
+var laOperacion: Operacion = .suma
+print("Mi operacion se llama \(laOperacion) con el símbolo \(laOperacion.rawValue)")
+
+if let nuevaOperacion = Operacion(rawValue: "*") {
+    print("Mi nueva operacion se llama \(nuevaOperacion) con el símbolo \(nuevaOperacion.rawValue)")
+    
+    let resultadoOperacion = nuevaOperacion.operar(numeroA: 4, numeroB: 2)
+    print(resultadoOperacion)
+}
+
+enum TipoTarjeta: Int {
+    
+    case visa = 0
+    case mastercard = 1
+    case diners = 2
+    
+    var imagen: UIImage? {
+        switch self {
+        case .visa:
+            return UIImage(named: "visa")
+        case .mastercard:
+            return UIImage(named: "mastercard")
+        case .diners:
+            return UIImage(named: "diners")
+        }
+    }
+}
+
+// EMNUM TIPO 3: VALORES ASOCIADOS
+enum Acciones {
+    case caminar(Int, Int)
+    case atacar(String, Int)
+    case recolectar(Int)
+    case descanzar
+}
+
+struct Personaje {
+    var nombre: String = ""
+    var vida: Int = 100
+    var ataque: Int = 2
+    var posicion: CGPoint = CGPoint(x: 0, y: 0)
+    
+    mutating func ejecutar(accion: Acciones) {
+        switch accion {
+        case .caminar(let x, let y):
+            posicion.x += CGFloat(x)
+            posicion.y += CGFloat(y)
+        
+        case .atacar(let enemigo, let vidaEnemigo):
+            print("Ataco al enemigo \(enemigo): \(vidaEnemigo - ataque)")
+        
+        case .recolectar(let item):
+            vida += item
+            
+        case .descanzar:
+            vida += 1
+        }
+    }
+}
+
+var avatar = Personaje()
+avatar.ejecutar(accion: .caminar(2, 3))
+avatar.ejecutar(accion: .recolectar(90))
+avatar.ejecutar(accion: .atacar("Steve", 200))
+
